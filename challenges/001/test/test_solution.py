530ecba8446fc87995d62df422e1edb504cb8cfb
jmr-enola <jmr.enola@gmail.com>
2024-12-15 23:53:14 +0800

    pytest setup and challenge 001

---
+++ challenges/001/test/test_solution.py
@@ -0,0 +1,33 @@
+from solution import solution01, solution02
+
+def test_solution01():
+    #Test Condition 1
+    assert solution01(17, [10, 15, 3, 7]) == True
+
+    #Test Condition 2
+    assert solution01(12, [10, 15, 3, 7]) == False
+
+    #Test Condition 3
+    assert solution01(14, [10, 15, 3, 7, 7, 4]) == True
+
+    #Test Condition 4
+    assert solution01(0, [0, 15, 3, 7, 7, 4]) == False
+
+    #Test Condition 5
+    assert solution02(12, [5, 15, 3, 7, 7, 4]) == True
+
+def test_solution02():
+    #Test Condition 1
+    assert solution02(17, [10, 15, 3, 7]) == True
+
+    #Test Condition 2
+    assert solution02(12, [10, 15, 3, 7]) == False
+
+    #Test Condition 3
+    assert solution02(14, [10, 15, 3, 7, 7, 4]) == True
+
+    #Test Condition 4
+    assert solution02(0, [0, 15, 3, 7, 7, 4]) == False
+
+    #Test Condition 5
+    assert solution02(12, [5, 15, 3, 7, 7, 4]) == True
