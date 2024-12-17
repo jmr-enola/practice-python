6e65c2d87ef100d32fb8d3a35d3830566508242f
jmr-enola <jmr.enola@gmail.com>
2024-12-17 18:04:41 +0800

    challenge d0002 no division solution

--- challenges/0002[Hard]/test_solution_d0002.py
+++ challenges/0002[Hard]/test_solution_d0002.py
@@ -1,4 +1,4 @@
-from solution_d0002 import solution01
+from solution_d0002 import solution01, solution02
 
 def test_solution01():
     #Test Condition 1
@@ -8,5 +8,16 @@ def test_solution01():
 
     #Test Condition 2
     result = solution01([3, 2, 1])
+    excepted = [2, 3, 6]
+    assert result == excepted
+
+def test_solution02():
+    #Test Condition 1
+    result = solution02([1, 2, 3, 4, 5])
+    excepted = [120, 60, 40, 30, 24]
+    assert result == excepted
+
+    #Test Condition 2
+    result = solution02([3, 2, 1])
     excepted = [2, 3, 6]
     assert result == excepted
