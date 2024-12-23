74d16c105d4696f413fd72383dbcace75e8c79cb
jmr-enola <jmr.enola@gmail.com>
2024-12-23 15:36:16 +0800

    challenge 0009 solution

---
+++ challenges/0009[Hard]/test_solution_d0009.py
@@ -0,0 +1,6 @@
+from solution_d0009 import solution01
+
+def test_solution01():
+    assert solution01([2, 4, 6, 2, 5]) == 13
+    assert solution01([5, 1, 1, 5]) == 10
+    assert solution01([2, 12, 6, 2, 5]) == 17
